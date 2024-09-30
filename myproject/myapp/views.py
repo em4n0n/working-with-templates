8a8f392d3118b032d571bb49a213fd99fa1ff05d
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-29 20:26:18 -0700

    Define a menu function

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    render(request, 'home.html')
+    render(request, "home.html")
+    
+def menu(request):
+    render(request, "menu.html")
     
