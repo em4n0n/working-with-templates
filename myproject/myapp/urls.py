f84d2615f6513769222322ce4b9f7669f48718fa
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-30 19:58:03 -0700

    Update url patterns in urls.py

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('about/', views.about),
+    path('book/', views.book),
+    path('home/', views.home),
+    path('menu', views.menu),
+]
